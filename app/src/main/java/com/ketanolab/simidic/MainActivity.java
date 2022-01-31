f52baf9247a07b970329f240653e9d445efd427e
pedro teran gezn <pedro.gezn@contractor.brightinsight.com>
2022-01-31 16:24:54 -0300

    removed unsuded menu drawer

--- app/src/main/java/com/ketanolab/simidic/MainActivity.java
+++ app/src/main/java/com/ketanolab/simidic/MainActivity.java
@@ -1,14 +1,11 @@
 package com.ketanolab.simidic;
 
-import java.util.ArrayList;
-
-
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
-
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -23,252 +20,221 @@ import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
-//LIBRERIAS IMPORTADAS PARA NOTIFICACION PUSH
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
-
-import android.os.AsyncTask;
-
-
-//
-
-
-import com.ketanolab.simidic.UIUtils.MenuDrawer;
-import com.ketanolab.simidic.adapters.MenuListAdapter;
 import com.ketanolab.simidic.util.Constants;
 import com.ketanolab.simidic.util.Dictionaries;
 import com.ketanolab.simidic.util.Util;
 
+import java.util.ArrayList;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
+public class MainActivity extends AppCompatActivity implements ListView.OnItemClickListener, ActionBar.OnNavigationListener {
 
-public class MainActivity extends AppCompatActivity implements  ListView.OnItemClickListener, ActionBar.OnNavigationListener{
+    // Paths dictionaries
+    private ArrayList<String> pathsDictionaries;
 
-	// Paths dictionaries
-	private ArrayList<String> pathsDictionaries;
+    // Navigation
+    private int itemSelectedNavigation = 0;
+    private ArrayAdapter<String> listNavigationAdapter;
 
-	// Navigation
-	private int itemSelectedNavigation = 0;
-	private ArrayAdapter<String> listNavigationAdapter;
+    // List words
+    private ListView wordsListView;
+    private SimpleCursorAdapter WordsSimpleCursorAdapter;
+    SQLiteDatabase db;
 
-	// List words
-	private ListView wordsListView;
-	private SimpleCursorAdapter WordsSimpleCursorAdapter;
-	SQLiteDatabase db;
+    // EditText
+    private EditText askBox;
 
-	//list menu
-	private ListView menuListView;
-	private MenuListAdapter menuListAdapter;
-	// EditText
-	private EditText askBox;
+    AsyncTask<Void, Void, Void> mRegisterTask;
 
+    // END NOTIFICACION PUSH
 
-	//MenuDrawer Button
-	private ActionBarDrawerToggle mMenu;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(Constants.DEBUG, "onCreate()");
+        setContentView(R.layout.activity_main);
 
-	AsyncTask<Void, Void, Void> mRegisterTask;
+        Context context = getSupportActionBar().getThemedContext();
+        listNavigationAdapter = new ArrayAdapter<String>(context,
+                R.layout.action_bar_spinner_list_item);
+        listNavigationAdapter
+                .setDropDownViewResource(R.layout.action_bar_spinner_list_item);
 
-	// END NOTIFICACION PUSH
+        final android.app.ActionBar actionBar = getActionBar();
+        // List
+        wordsListView = (ListView) findViewById(R.id.lista);
+        wordsListView.setOnItemClickListener(this);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); // Hide title
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        getSupportActionBar().setListNavigationCallbacks(listNavigationAdapter,
+                this);
+        askBox = findViewById(R.id.caja_consulta);
+        askBox.addTextChangedListener(new TextWatcher() {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Log.i(Constants.DEBUG, "onCreate()");
-		setContentView(R.layout.activity_main);
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                if (s.toString().length() > 0) {
+                    WordsSimpleCursorAdapter.getFilter().filter(s.toString());
+                }
+            }
 
-		//menuListView.setOnItemClickListener(this);
-		// Navigation
-		Context context = getSupportActionBar().getThemedContext();
-		listNavigationAdapter = new ArrayAdapter<String>(context,
-				R.layout.action_bar_spinner_list_item);
-		listNavigationAdapter
-				.setDropDownViewResource(R.layout.action_bar_spinner_list_item);
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
 
-		final android.app.ActionBar actionBar = getActionBar();
-		// List
-		wordsListView = (ListView) findViewById(R.id.lista);
-		wordsListView.setOnItemClickListener(this);
-		getSupportActionBar().setDisplayShowTitleEnabled(false); // Hide title
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		getSupportActionBar().setHomeButtonEnabled(true);
-		getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-		getSupportActionBar().setListNavigationCallbacks(listNavigationAdapter,
-				this);
-		DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-		mMenu = MenuDrawer.getMenuDrawer(this,getSupportActionBar(),drawerLayout);
-		menuListView = (ListView)findViewById(R.id.list_slidermenu);
-		menuListView.setAdapter(new MenuListAdapter(this));
-		askBox = (EditText) findViewById(R.id.caja_consulta);
-		askBox.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+            }
+        });
 
-			public void onTextChanged(CharSequence s, int start, int before,
-									  int count) {
-				if (s.toString().length() > 0) {
-					WordsSimpleCursorAdapter.getFilter().filter(s.toString());
-				}
-			}
+    }
 
-			public void beforeTextChanged(CharSequence s, int start, int count,
-										  int after) {
-			}
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Constants.DEBUG, "onStart()");
 
-			public void afterTextChanged(Editable s) {
-			}
-		});
+        // Get dictionaries valid
+        pathsDictionaries = Dictionaries.scanDictionariesAndValidation(this);
+        // Put Navigation
+        putDataDictionariesInNavigation();
+        // Load words
+        Log.i(Constants.DEBUG, "Cargando palabras...");
+        if (pathsDictionaries.size() > 0) {
+            db = SQLiteDatabase.openOrCreateDatabase(
+                    pathsDictionaries.get(itemSelectedNavigation), null);
+            loadAllWords();
+        }
+        // Re-query
+        String s = askBox.getText().toString();
+        if (s.toString().length() > 0) {
+            WordsSimpleCursorAdapter.getFilter().filter(s.toString());
+        }
+    }
 
-	}
+    private void loadAllWords() {
+        final String[] columns = {"_id", "word", "summary"};
+        int[] to = {0, R.id.word_item, R.id.meaning_item};
+        // Cursor cursor = db.rawQuery("SELECT _id, word, summary FROM words",
+        // null);
+        Cursor cursor = db
+                .query("words", columns, null, null, null, null, null);
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-		Log.i(Constants.DEBUG, "onStart()");
+        WordsSimpleCursorAdapter = new SimpleCursorAdapter(this,
+                R.layout.word_list_item, cursor, columns, to, 0);
 
-		// Get dictionaries valid
-		pathsDictionaries = Dictionaries.scanDictionariesAndValidation(this);
-		// Put Navigation
-		putDataDictionariesInNavigation();
-		// Load words
-		Log.i(Constants.DEBUG, "Cargando palabras...");
-		if (pathsDictionaries.size() > 0) {
-			db = SQLiteDatabase.openOrCreateDatabase(
-					pathsDictionaries.get(itemSelectedNavigation), null);
-			loadAllWords();
-		}
-		// Re-query
-		String s = askBox.getText().toString();
-		if (s.toString().length() > 0) {
-			WordsSimpleCursorAdapter.getFilter().filter(s.toString());
-		}
-	}
+        WordsSimpleCursorAdapter.setFilterQueryProvider(new FilterQueryProvider() {
+            public Cursor runQuery(CharSequence constraint) {
 
-	private void loadAllWords() {
-		final String[] columns = { "_id", "word", "summary" };
-		int[] to = { 0, R.id.word_item, R.id.meaning_item };
-		// Cursor cursor = db.rawQuery("SELECT _id, word, summary FROM words",
-		// null);
-		Cursor cursor = db
-				.query("words", columns, null, null, null, null, null);
+                String word = "%" + constraint.toString() + "%";
+                Cursor cursor = null;
+                if (pathsDictionaries.get(itemSelectedNavigation)
+                        .contains("gn")) {
+                    cursor = db
+                            .rawQuery(
+                                    "SELECT _id, word, summary FROM words WHERE replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(word,'�','a'),'�','e'),'�','i'),'�','o'),'�','u'),'(',''),')',''),'�','n'),'�','i'),'�','a'),'�','e'),'�','o'),'�','u') LIKE ?",
+                                    new String[]{word});
+                } else {
+                    cursor = db
+                            .rawQuery(
+                                    "SELECT _id, word, summary FROM words WHERE replace(replace(replace(replace(replace(replace(replace(word,'�','a'),'�','e'),'�','i'),'�','o'),'�','u'),'�','n'),'�','a') LIKE ?",
+                                    new String[]{word});
+                }
 
-		WordsSimpleCursorAdapter = new SimpleCursorAdapter(this,
-				R.layout.word_list_item, cursor, columns, to, 0);
+                return cursor;
+            }
+        });
 
-		WordsSimpleCursorAdapter.setFilterQueryProvider(new FilterQueryProvider() {
-			public Cursor runQuery(CharSequence constraint) {
+        wordsListView.setAdapter(WordsSimpleCursorAdapter);
 
-				String word = "%" + constraint.toString() + "%";
-				Cursor cursor = null;
-				if (pathsDictionaries.get(itemSelectedNavigation)
-						.contains("gn")) {
-					cursor = db
-							.rawQuery(
-									"SELECT _id, word, summary FROM words WHERE replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(word,'�','a'),'�','e'),'�','i'),'�','o'),'�','u'),'(',''),')',''),'�','n'),'�','i'),'�','a'),'�','e'),'�','o'),'�','u') LIKE ?",
-									new String[]{word});
-				} else {
-					cursor = db
-							.rawQuery(
-									"SELECT _id, word, summary FROM words WHERE replace(replace(replace(replace(replace(replace(replace(word,'�','a'),'�','e'),'�','i'),'�','o'),'�','u'),'�','n'),'�','a') LIKE ?",
-									new String[]{word});
-				}
+    }
 
-				return cursor;
-			}
-		});
+    private void putDataDictionariesInNavigation() {
+        listNavigationAdapter.clear();
+        for (int i = 0; i < pathsDictionaries.size(); i++) {
+            SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(
+                    pathsDictionaries.get(i), null);
+            String[] nameAndAuthor = Util.getNameAndAuthorDictionary(db);
+            listNavigationAdapter.add(nameAndAuthor[0]); // Get name
+            db.close();
+        }
+        listNavigationAdapter.notifyDataSetChanged();
+    }
 
-		wordsListView.setAdapter(WordsSimpleCursorAdapter);
+    public boolean onNavigationItemSelected(int position, long id) {
+        itemSelectedNavigation = position;
+        if (pathsDictionaries.size() > 0) {
+            if (db != null) {
+                db.close();
+            }
+            db = SQLiteDatabase.openOrCreateDatabase(
+                    pathsDictionaries.get(itemSelectedNavigation), null);
+            loadAllWords();
+        }
+        // Re-query
+        String word = askBox.getText().toString();
+        if (word.length() > 0) {
+            WordsSimpleCursorAdapter.getFilter().filter(word);
+        }
+        return true;
+    }
 
-	}
+    // ************************* MENU *************************
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
 
-	private void putDataDictionariesInNavigation() {
-		listNavigationAdapter.clear();
-		for (int i = 0; i < pathsDictionaries.size(); i++) {
-			SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(
-					pathsDictionaries.get(i), null);
-			String[] nameAndAuthor = Util.getNameAndAuthorDictionary(db);
-			listNavigationAdapter.add(nameAndAuthor[0]); // Get name
-			db.close();
-		}
-		listNavigationAdapter.notifyDataSetChanged();
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_download:
+                Intent intentDescarga = new Intent(this, DownloadsActivity.class);
+                startActivity(intentDescarga);
+                break;
+            case R.id.menu_favorites:
+                Intent intentFavoritos = new Intent(this, FavoritesActivity.class);
+                startActivity(intentFavoritos);
+                break;
+            case R.id.menu_information:
+                Intent intentCreditos = new Intent(this, CreditsActivity.class);
+                startActivity(intentCreditos);
+                break;
+            case R.id.menu_dictionaries:
+                Intent intentDiccionarios = new Intent(this,
+                        DiccionariesActivity.class);
+                startActivity(intentDiccionarios);
+                break;
+        }
+        return true;
+    }
 
-	public boolean onNavigationItemSelected(int position, long id) {
-		itemSelectedNavigation = position;
-		if (pathsDictionaries.size() > 0) {
-			if (db != null) {
-				db.close();
-			}
-			db = SQLiteDatabase.openOrCreateDatabase(
-					pathsDictionaries.get(itemSelectedNavigation), null);
-			loadAllWords();
-		}
-		// Re-query
-		String word = askBox.getText().toString();
-		if (word.length() > 0) {
-			WordsSimpleCursorAdapter.getFilter().filter(word);
-		}
-		return true;
-	}
+    // ************************* ITEMCLICK *************************
+    public void onItemClick(AdapterView<?> arg0, View v, int posicion, long id) {
+        // Start WordActivity
+        String word = ((TextView) v.findViewById(R.id.word_item)).getText()
+                .toString();
+        Intent intent = new Intent(this, WordActivity.class);
+        intent.putExtra("path", pathsDictionaries.get(getSupportActionBar()
+                .getSelectedNavigationIndex()));
+        intent.putExtra("word", word);
+        startActivity(intent);
+    }
 
-	// ************************* MENU *************************
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		/*if (mMenu.onOptionsItemSelected(item)) {
-			return true;
-		}*/
-		switch (item.getItemId()) {
-		case R.id.menu_download:
-			Intent intentDescarga = new Intent(this, DownloadsActivity.class);
-			startActivity(intentDescarga);
-			break;
-		case R.id.menu_favorites:
-			Intent intentFavoritos = new Intent(this, FavoritesActivity.class);
-			startActivity(intentFavoritos);
-			break;
-		case R.id.menu_information:
-			Intent intentCreditos = new Intent(this, CreditsActivity.class);
-			startActivity(intentCreditos);
-			break;
-		case R.id.menu_dictionaries:
-			Intent intentDiccionarios = new Intent(this,
-					DiccionariesActivity.class);
-			startActivity(intentDiccionarios);
-			break;
-		}
-		return true;
-	}
-
-	// ************************* ITEMCLICK *************************
-	public void onItemClick(AdapterView<?> arg0, View v, int posicion, long id) {
-		// Start WordActivity
-		String word = ((TextView) v.findViewById(R.id.word_item)).getText()
-				.toString();
-		Intent intent = new Intent(this, WordActivity.class);
-		intent.putExtra("path", pathsDictionaries.get(getSupportActionBar()
-				.getSelectedNavigationIndex()));
-		intent.putExtra("word", word);
-		startActivity(intent);
-	}
-
-	@Override
-	protected void onStop() {
-		super.onStop();
-		Log.i(Constants.DEBUG, "onStop()");
-		if (WordsSimpleCursorAdapter != null) {
-			WordsSimpleCursorAdapter.getCursor().close();
-			WordsSimpleCursorAdapter = null;
-		}
-		if (db != null) {
-			db.close();
-		}
-	}
-
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(Constants.DEBUG, "onStop()");
+        if (WordsSimpleCursorAdapter != null) {
+            WordsSimpleCursorAdapter.getCursor().close();
+            WordsSimpleCursorAdapter = null;
+        }
+        if (db != null) {
+            db.close();
+        }
+    }
 
 
 }
