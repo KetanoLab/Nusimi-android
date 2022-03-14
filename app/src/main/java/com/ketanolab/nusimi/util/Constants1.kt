72b8ed2c367c7771e3a7ac6fb749a337dc348618
ketanolab <ketanolab@gmail.com>
2022-03-14 16:19:46 -0300

    fixed repository path

--- app/src/main/java/com/ketanolab/nusimi/util/Constants1.kt
+++ app/src/main/java/com/ketanolab/nusimi/util/Constants1.kt
@@ -2,6 +2,6 @@ package com.ketanolab.nusimi.util
 
 object Constants {
     const val URL_JSON =
-        "http://ketanolab.com/simidic/simidic.json"
+        "https://ketanolab.com/simidic/simidic.json"
     const val DEBUG = "simidic"
 }
